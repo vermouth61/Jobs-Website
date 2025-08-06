90654dc350cdb7f872421a3a3206482d3014b625
Diaa <diaa@Diaas-MacBook-Air.local>
2025-08-06 20:31:28 +0300

    Edit Margins in Worker Account Page + Profile

--- src/Layout/profileLayout.vue
+++ src/Layout/profileLayout.vue
@@ -5,7 +5,7 @@ import SideBar from '../components/sideBar.vue';
 </script>
 <template>
   <NavBar />
-  <div dir="rtl" class="px-[24px] sm:px-[48px] md:px-[64px] lg:px-[100px] pt-[24px] flex gap-[20px]">
+  <div dir="rtl" class="px-[24px] sm:px-[48px] md:px-[64px] lg:px-[100px] pt-[24px] flex gap-[20px] mb-[128px]">
   <SideBar />
   <router-view />
   </div>
