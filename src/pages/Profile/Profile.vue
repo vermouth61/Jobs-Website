5c1cb0ebf1c551e789693c49e9ac180b3d940d23
Diaa <diaa@Diaas-MacBook-Air.local>
2025-08-07 10:01:05 +0300

    Create Profile Page

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -1,9 +1,16 @@
 <script setup>
-    
+import WorkerExperience from '../../components/WorkerExperience.vue';
+import WorkerSummary from '../../components/WorkerSummary.vue';
+import { workerExperienceArray } from '../../data/index.js';
+
 </script>
 
 <template>
- <div>
-    <h1>Profile Page</h1>
- </div>
+   <div>
+      <div class="flex flex-col gap-[20px]">
+            <WorkerSummary  workerName="أحمد محمد الشرقاوي" workerPosition="لحام أرجون و كهرباء" workerDescription="فني لحام محترف بخبرة 5 سنوات في اللحام الكهربائي ولحام الأرجون. أتميّز بدقة عالية في العمل، وحرص دائم على السلامة والجودة في تنفيذ المهام." />
+            <WorkerExperience :workerExperience="workerExperienceArray"/>
+      </div>
+
+   </div>
 </template>
