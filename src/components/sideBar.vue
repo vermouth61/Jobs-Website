979de55c9fa10ba959fde8c3a661c29921d3accf
Omar Elhussieny <omarelnahas61@gmail.com>
2025-08-08 07:26:39 +0300

    Finished jobs page and refactoring data

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,75 +1,131 @@
 <script setup>
-import { BsFillPersonFill } from 'vue-icons-plus/bs';
-import { TbLogout2, TbPhotoFilled } from 'vue-icons-plus/tb';
-import { MdPersonSearch } from 'vue-icons-plus/md';
-import { BiSolidBookmarkStar } from 'vue-icons-plus/bi';
-import { Io5SettingsSharp } from 'vue-icons-plus/io5';
-import Button from './ui/button.vue';
-import { AiOutlineMenuFold } from 'vue-icons-plus/ai';
-import { HiMiniUserGroup } from 'vue-icons-plus/hi2';
-import { useRouter } from 'vue-router';
+import { BsFillPersonFill } from "vue-icons-plus/bs";
+import { TbLogout2, TbPhotoFilled } from "vue-icons-plus/tb";
+import { MdPersonSearch } from "vue-icons-plus/md";
+import { BiSolidBookmarkStar } from "vue-icons-plus/bi";
+import { Io5SettingsSharp } from "vue-icons-plus/io5";
+import Button from "./ui/button.vue";
+import { AiOutlineMenuFold } from "vue-icons-plus/ai";
+import { HiMiniUserGroup } from "vue-icons-plus/hi2";
+import { useRouter } from "vue-router";
 const router = useRouter();
 const logout = () => {
-    localStorage.removeItem('userToken');
-    router.push('/');
-}
+  localStorage.removeItem("userToken");
+  router.push("/");
+};
 </script>
 
 <template>
-<div dir="rtl" class="bg-[#F6F8FA] lg:border-[1px] lg:border-[#B8BCC7] lg:rounded-[8px] px-[16px] py-[12px] lg:py-[24px] w-full lg:w-[295px] flex flex-col gap-[16px]">
-        <input id="side-toggle" type="checkbox" class="peer hidden" />
+  <div
+    dir="rtl"
+    class="bg-[#F6F8FA] lg:border-[1px] lg:border-[#B8BCC7] lg:rounded-[8px] px-[16px] py-[12px] lg:py-[24px] w-full lg:w-[295px] flex flex-col gap-[16px]"
+  >
+    <input id="side-toggle" type="checkbox" class="peer hidden" />
     <div class="flex items-center gap-3 lg:hidden">
-
       <label
         for="side-toggle"
         class="cursor-pointer inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100"
       >
-<AiOutlineMenuFold />
+        <AiOutlineMenuFold />
       </label>
     </div>
-    <div class="w-full mt-4 peer-checked:flex hidden flex-col gap-[16px] lg:flex lg:flex-col lg:items-start lg:w-full lg:mt-0">
-    <router-link to="/profile" class=" text-[#36394A] w-full text-[16px] font-[400] flex items-center">
-        <span class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
-        :class="{ 'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent': $route.path === '/profile' }">
-        <BsFillPersonFill class="w-[22px]"/>البيانات الشخصية
+    <div
+      class="w-full mt-4 peer-checked:flex hidden flex-col gap-[16px] lg:flex lg:flex-col lg:items-start lg:w-full lg:mt-0"
+    >
+      <router-link
+        to="/profile"
+        class="text-[#36394A] w-full text-[16px] font-[400] flex items-center"
+      >
+        <span
+          class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
+          :class="{
+            'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent':
+              $route.path === '/profile',
+          }"
+        >
+          <BsFillPersonFill class="w-[22px]" />البيانات الشخصية
         </span>
-    </router-link>
-    <router-link to="/job-applicants" class=" text-[#36394A] w-full text-[16px] font-[400] flex items-center">
-        <span class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
-        :class="{ 'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent': $route.path === '/job-applicants' }">
-        <HiMiniUserGroup class="w-[22px]"/>المتقدمين للعمل
+      </router-link>
+      <router-link
+        to="/job-applicants"
+        class="text-[#36394A] w-full text-[16px] font-[400] flex items-center"
+      >
+        <span
+          class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
+          :class="{
+            'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent':
+              $route.path === '/job-applicants',
+          }"
+        >
+          <HiMiniUserGroup class="w-[22px]" />المتقدمين للعمل
         </span>
-    </router-link>
-    <router-link to="/portfolio" class="text-[#36394A] w-full text-[16px] font-[400] flex items-center" >
-        <span class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
-        :class="{ 'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent': $route.path === '/portfolio'}">
-        <TbPhotoFilled class="w-[22px]"/>معرض الأعمال
+      </router-link>
+      <router-link
+        to="/portfolio"
+        class="text-[#36394A] w-full text-[16px] font-[400] flex items-center"
+      >
+        <span
+          class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
+          :class="{
+            'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent':
+              $route.path === '/portfolio',
+          }"
+        >
+          <TbPhotoFilled class="w-[22px]" />معرض الأعمال
         </span>
-    </router-link>
-    <router-link to="/hiring-workers" class=" text-[#36394A] w-full text-[16px] font-[400] flex items-center">
-        <span class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
-        :class="{ 'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent': $route.path === '/hiring-workers'}">
-        <MdPersonSearch class="w-[22px]"/>تعيين عُمال
+      </router-link>
+      <router-link
+        to="/hiring-workers"
+        class="text-[#36394A] w-full text-[16px] font-[400] flex items-center"
+      >
+        <span
+          class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
+          :class="{
+            'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent':
+              $route.path === '/hiring-workers',
+          }"
+        >
+          <MdPersonSearch class="w-[22px]" />تعيين عُمال
         </span>
-    </router-link>
-    <router-link to="/saved-jobs" class=" text-[#36394A] w-full text-[16px] font-[400] flex items-center">
-        <span class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
-        :class="{ 'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent': $route.path === '/saved-jobs'}">
-        <BiSolidBookmarkStar class="w-[22px]"/>الوظائف المحفوظة
+      </router-link>
+      <router-link
+        to="/saved-jobs"
+        class="text-[#36394A] w-full text-[16px] font-[400] flex items-center"
+      >
+        <span
+          class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
+          :class="{
+            'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent':
+              $route.path === '/saved-jobs',
+          }"
+        >
+          <BiSolidBookmarkStar class="w-[22px]" />الوظائف المحفوظة
         </span>
-    </router-link>
-    <router-link to="/profile-settings" class=" text-[#36394A] w-full text-[16px] font-[400] flex items-center">
-        <span class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
-        :class="{ 'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent': $route.path === '/profile-settings'}">
-        <Io5SettingsSharp class="w-[22px]"/>إعدادات الحساب
+      </router-link>
+      <router-link
+        to="/profile-settings"
+        class="text-[#36394A] w-full text-[16px] font-[400] flex items-center"
+      >
+        <span
+          class="flex items-center gap-[8px] w-full h-full p-[16px] rounded-[12px] border-[1px] border-[#B8BCC7]"
+          :class="{
+            'bg-[#CCDDFF] text-[#246BFD] font-[600] border-transparent':
+              $route.path === '/profile-settings',
+          }"
+        >
+          <Io5SettingsSharp class="w-[22px]" />إعدادات الحساب
         </span>
-    </router-link>
-    <div class="w-full"> 
-        <Button buttonType="secondary" @clickButton="logout" extraClass="w-full text-start rounded-[12px] px-[20px] flex gap-[8px]">
-            <TbLogout2 class="inline-block w-[22px]"/>
-            تسجيل الخروج
+      </router-link>
+      <div class="w-full">
+        <Button
+          buttonType="secondary"
+          @clickButton="logout"
+          extraClass="w-full text-start rounded-[12px] px-[20px] flex gap-[8px]"
+        >
+          <TbLogout2 class="inline-block w-[22px]" />
+          تسجيل الخروج
         </Button>
+      </div>
     </div>
-    </div>
-</div>
+  </div>
 </template>
--- src/pages/Jobs.vue
+++ src/pages/Jobs.vue
@@ -1,7 +1,195 @@
 <script setup>
+import { useRoute } from "vue-router";
+import { ref, watch, onMounted } from "vue";
+import { HiMiniAdjustmentsHorizontal } from "vue-icons-plus/hi2";
+import RecentJobsCard from "../components/recentJobsCard.vue";
+import data from "../data/data.json";
+import MyInput from "../components/ui/MyInput.vue";
+import { GrSearch } from "vue-icons-plus/gr";
+import Select from "../components/ui/select.vue";
+import { fields, governorates, levels, salaries } from "../data";
 
+const route = useRoute();
+const allJobs = ref(data.recentJobsArray);
+const search = ref("");
+const debouncedSearch = ref(""); // new
+const selectedField = ref("");
+const selectedGovernorate = ref("");
+const selectedLevel = ref("");
+const selectedSalary = ref("");
+let debounceTimer;
+
+onMounted(() => {
+    if (route.query.search) {
+        search.value = route.query.search;
+        debouncedSearch.value = route.query.search;
+    }
+});
+const filteredJobs = ref([]);
+
+const filterJobs = () => {
+    const result = allJobs.value.filter((job) => {
+    const title = job.jobTitle;
+    const tags = job.tags;
+    const location = job.location;
+
+    const matchesSearch =
+      !debouncedSearch.value ||
+      tags.includes(debouncedSearch.value) ||
+      title.includes(debouncedSearch.value);
+
+    const matchesField =
+      !selectedField.value ||
+      tags.includes(selectedField.value) ||
+      title.includes(selectedField.value);
+
+    const matchesGovernorate =
+      !selectedGovernorate.value ||
+      location.includes(selectedGovernorate.value);
+
+    const matchesLevel =
+      !selectedLevel.value || job.desires[1] === selectedLevel.value;
+
+    const matchesSalary =
+      !selectedSalary.value ||
+      (job.dailyRate >= +selectedSalary.value.split("-")[0] &&
+        job.dailyRate <= +selectedSalary.value.split("-")[1]);
+
+    return (
+      matchesSearch &&
+      matchesField &&
+      matchesGovernorate &&
+      matchesLevel &&
+      matchesSalary
+    );
+  });
+
+  filteredJobs.value = result;
+};
+watch(search, (val) => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    debouncedSearch.value = val;
+  }, 500);
+});
+watch(
+  [
+    debouncedSearch,
+    selectedField,
+    selectedGovernorate,
+    selectedLevel,
+    selectedSalary
+  ],
+  filterJobs,
+  { immediate: true }
+);
 </script>
 
-<template>
 
+<template>
+  <div dir="rtl" class="container mx-auto mb-[128px] mt-12">
+    <div class="flex flex-col lg:flex-row gap-[20px]">
+      <div
+        dir="rtl"
+        class="bg-[#F6F8FA] lg:border-[1px] lg:border-[#B8BCC7] lg:rounded-[8px] p-[24px] h-fit w-full lg:max-w-[400px] flex flex-col gap-[24px]"
+      >
+        <input id="side-toggle" type="checkbox" class="peer hidden" />
+        <div class="flex items-center gap-3 lg:hidden">
+          <label
+            for="side-toggle"
+            class="cursor-pointer inline-flex items-center p-2 justify-center text-sm text-[#246BFD] rounded-lg hover:bg-gray-100"
+          >
+            <div class="flex items-center gap-1">
+              <HiMiniAdjustmentsHorizontal class="w-[26px]" />
+              <span class="font-[600] text-[18px]">تصفية الوظائف</span>
+            </div>
+          </label>
+        </div>
+        <div
+          class="w-full mt-4 peer-checked:flex hidden flex-col gap-[16px] lg:flex lg:flex-col lg:items-start lg:w-full lg:mt-0"
+        >
+          <div class="hidden lg:flex items-center gap-2 text-[#246BFD]">
+            <HiMiniAdjustmentsHorizontal class="w-[26px]" />
+            <span class="font-[600] text-[24px]">تصفية الوظائف</span>
+          </div>
+          <div class="flex flex-col w-full relative gap-[4px]">
+            <label for="search" class="text-[16px] font-[600]"
+              >ابحث بالشغلانة</label
+            >
+            <MyInput
+              id="search"
+              v-model="search"
+              type="text"
+              placeholder="اسم الشغلانة"
+              extraClass="w-full bg-white border-[#D1D5DB] focus:outline-none focus:border-[#246BFD] text-base"
+              required
+            />
+            <GrSearch class="absolute left-[12px] top-1/2 text-[#666D80]" />
+          </div>
+          <div class="flex flex-col w-full relative gap-[4px]">
+            <label for="search" class="text-[16px] font-[600]"
+              >مجال العمل</label
+            >
+            <Select
+              :options="fields"
+              defaultSelected="مجال عملك"
+              v-model="selectedField"
+            />
+          </div>
+          <div class="flex flex-col w-full relative gap-[4px]">
+            <label for="search" class="text-[16px] font-[600]">المحافظة</label>
+            <Select
+              :options="governorates"
+              defaultSelected="اختر المحافظة"
+              v-model="selectedGovernorate"
+            />
+          </div>
+          <div class="flex flex-col w-full relative gap-[4px]">
+            <label for="search" class="text-[16px] font-[600]"
+              >مستوى الخبرة</label
+            >
+            <Select
+              :options="levels"
+              defaultSelected="جميع المستويات"
+              v-model="selectedLevel"
+            />
+          </div>
+          <div class="flex flex-col w-full relative gap-[4px]">
+            <label for="search" class="text-[16px] font-[600]"
+              >نطاق الراتب اليومي</label
+            >
+            <Select
+              :options="salaries"
+              defaultSelected="الكل"
+              v-model="selectedSalary"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1
+          v-if="route.query.search"
+          class="text-[#1F2937] mb-[16px] text-[20px] font-[500]"
+        >
+          نتائج البحث عن: {{ route.query.search }}
+        </h1>
+        <div class="flex flex-wrap justify-center xl:justify-start gap-[20px]">
+          <RecentJobsCard
+            class="w-[358px]! h-fit"
+            v-if="filteredJobs.length"
+            v-for="job in filteredJobs"
+            :key="job.id"
+            :id="job.id"
+            :jobTitle="job.jobTitle"
+            :location="job.location"
+            :company="job.company"
+            :dailyRate="job.dailyRate"
+            :field="job.field"
+            :desires="job.desires"
+          />
+          <h1 v-else class="text-[#1F2937] text-[20px] font-[500]">لا يوجد وظائف</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
